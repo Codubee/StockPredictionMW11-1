695f7f411a884a4fb06a62db15b9ba4d51c10a9f
kNgo8249 <kenguin8249@gmail.com>
2020-12-10 18:42:25 -0600

    Implemented rough news endpoint to get company news

--- backend/index.js
+++ backend/index.js
@@ -14,4 +14,26 @@ app.listen(port,()=>{
     console.log('API is up and running')
 })
 
+app.get('/news', (req,res)=> {
+
+    let currentDate = new Date()
+    let weekAgo = new Date()
+    weekAgo.setDate(currentDate.getDate() - 7)
+
+    let weekAgoString = weekAgo.toISOString().slice(0, 10)
+    let currentString = currentDate.toISOString().slice(0, 10)
+
+    const headers = {headers:{'Authorization':process.env.API_KEY}}
+    axios.get(`https://finnhub.io/api/v1/company-news?symbol=AAPL&from=${weekAgoString}&to=${currentString}`, headers)  
+    .then(function(response) {
+       // console.log(response.data)
+        res.status(200).json(response.data);
+    })
+    .catch(function(error) {
+        console.log(error)
+        res.status(400).json({error:"An error occured"})
+    })
+    
+})
+
 module.exports = app;
