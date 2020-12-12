1a274a5811cac840160f315d00993d70e4cf1bca
kNgo8249 <kenguin8249@gmail.com>
2020-12-12 17:49:31 -0600

    Updated tests for index.js

--- backend/test/index_test.js
+++ backend/test/index_test.js
@@ -1,15 +1,25 @@
+const { expect } = require('chai');
 let chai = require('chai')
 let chaiHttp = require('chai-http')
 let app =  require('../index')
 chai.should();
 chai.use(chaiHttp);
 
-describe('Tests get enpoint at /',()=>{
-    it('Should return hello world',(done)=>{
+describe('Tests index.js',()=>{
+    it('/ endpoint should return hello world',(done)=>{
         chai.request(app)
         .get('/')
         .end((err,res)=>{
             res.text.should.be.eql('Hello world')
+            done();
+        })
+    })
+    
+    it('/news endpoint should return news about a company. Testing with AAPL', (done)=> {
+        chai.request(app)
+        .get('/news/AAPL')
+        .end((err,res)=>{
+            res.text.should.contain('AAPL')
             done();
         })
     })
