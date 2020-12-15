4fe69a54a7955c56b0ce3e84055d5e7f83ef8c95
Ryan Dimaranan <48656699+ryannd@users.noreply.github.com>
2020-12-15 00:20:35 -0600

    added routing to app.js and links to navbar (#6)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 import LandingPage from './components/LandingPage/LandingPage'
 import NavBar from './components/Navbar/Navbar'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <>
+      <Router>
       <NavBar/>
-      <LandingPage/>
+        <Switch>
+          <Route exact path="/" component={LandingPage}/>
+        </Switch>
+      </Router>
     </>
   );
 }
