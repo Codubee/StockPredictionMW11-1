9c63d8da9624cfa65216e9eab8e34e54d1e7a96c
Joshua Durana <rjdurana@gmail.com>
2020-12-16 17:00:45 -0600

    Created an endpoint for text comprehension

--- backend/index.js
+++ backend/index.js
@@ -36,4 +36,26 @@ app.get('/news/:name', (req,res)=> {
 
 })
 
+app.get('/news/:name', (req,res)=> {
+
+    let currentDate = new Date()
+    let weekAgo = new Date()
+    weekAgo.setDate(currentDate.getDate() - 7)
+    
+    let name = req.params.name
+    let weekAgoString = weekAgo.toISOString().slice(0, 10)
+    let currentString = currentDate.toISOString().slice(0, 10)
+
+    const headers = {headers:{'X-Finnhub-Token':process.env.FINNHUB_TOKEN}}
+    axios.get(`https://finnhub.io/api/v1/company-news?symbol=${name}&from=${weekAgoString}&to=${currentString}`, headers)  
+    .then(function(response) {
+        res.status(200).json(response.data);
+    })
+    .catch(function(error) {
+        console.log(error)
+        res.status(400).json({error:"An error occured"})
+    })
+
+})
+
 module.exports = app;
