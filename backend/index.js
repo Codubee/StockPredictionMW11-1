62aa734ea91c176767005a51e75069fbcb4b6396
kNgo8249 <kenguin8249@gmail.com>
2020-12-16 11:22:28 -0600

    Cb 11 newsapi (#7)
    * Implemented rough news endpoint to get company news
    
    * Updated news endpoint to take company name param
    
    * Updated tests for index.js
    
    * Changed name of api key

--- backend/index.js
+++ backend/index.js
@@ -14,4 +14,26 @@ app.listen(port,()=>{
     console.log('API is up and running')
 })
 
+app.get('/news/:name', (req,res)=> {
+
+    let currentDate = new Date()
+    let weekAgo = new Date()
+    weekAgo.setDate(currentDate.getDate() - 7)
+    
+    let name = req.params.name
+    let weekAgoString = weekAgo.toISOString().slice(0, 10)
+    let currentString = currentDate.toISOString().slice(0, 10)
+
+    const headers = {headers:{'X-Finnhub-Token':process.env.FINNHUB_TOKEN}}
+    axios.get(`https://finnhub.io/api/v1/company-news?symbol=${name}&from=${weekAgoString}&to=${currentString}`, headers)  
+    .then(function(response) {
+        res.status(200).json(response.data);
+    })
+    .catch(function(error) {
+        console.log(error)
+        res.status(400).json({error:"An error occured"})
+    })
+
+})
+
 module.exports = app;
