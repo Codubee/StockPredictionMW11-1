35065494980f88af586abec5b574034932e7086b
Joshua Durana <rjdurana@gmail.com>
2020-12-16 17:45:27 -0600

    endpoint for comprehend

--- backend/index.js
+++ backend/index.js
@@ -36,26 +36,17 @@ app.get('/news/:name', (req,res)=> {
 
 })
 
-app.get('/news/:name', (req,res)=> {
+app.post('/comprehend', (req,res)=>{
+    var json = req.body;
 
-    let currentDate = new Date()
-    let weekAgo = new Date()
-    weekAgo.setDate(currentDate.getDate() - 7)
-    
-    let name = req.params.name
-    let weekAgoString = weekAgo.toISOString().slice(0, 10)
-    let currentString = currentDate.toISOString().slice(0, 10)
-
-    const headers = {headers:{'X-Finnhub-Token':process.env.FINNHUB_TOKEN}}
-    axios.get(`https://finnhub.io/api/v1/company-news?symbol=${name}&from=${weekAgoString}&to=${currentString}`, headers)  
-    .then(function(response) {
+    axios.post('https://codubee-api.herokuapp.com/comprehension', json)
+    .then(function(response){
         res.status(200).json(response.data);
     })
-    .catch(function(error) {
+    .catch(function(error){
         console.log(error)
-        res.status(400).json({error:"An error occured"})
+        res.status(400).json({error: "An error occurred"})
     })
-
 })
 
 module.exports = app;
