6f7e7926981258eeb6f7459a9d6ab705a9884bb1
Venkata Bapanapalli <46755666+vishnubapana@users.noreply.github.com>
2020-12-19 13:10:21 -0600

    Cb 10 call stock api (#8)
    * implemented stock-data api
    
    * fixed minor errors
    
    * updated index.js with sotck api implementation
    
    * index.js updated to require stock name in the endpoint from user
    
    * changed the name of the API_KEY variable to be unique.
    
    * Updated index.js to print data to the page and added function toUpperCase

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,20 @@ app.get('/',(req,res)=>{
     res.send('Hello world');
 })
 
+app.get('/stockname/:NAME',(req, res)=>{
+    const headers = {headers:{'Authorization':process.env.QUOTE_API_KEY}}
+    let NAME = (req.params.NAME).toUpperCase();
+    axios.get(`https://finnhub.io/api/v1/quote?symbol=${NAME}&token=bv6jgev48v6s9eue2ndg`, headers)
+    .then(function(response){
+        console.log(response);
+        res.status(200).json(response.data);
+    })
+    .catch(function(error){
+        console.log(error)
+        res.status(400).json({error:"An error occurred"})
+    })
+})
+
 app.listen(port,()=>{
     console.log('API is up and running')
 })
