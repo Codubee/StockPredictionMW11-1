5ae7a7a1982270f9606bc06eb8a40f64d5e8ce85
Vishnu Bapanapalli <vishnubapana@gmail.com>
2021-01-05 12:59:53 -0600

    implemented Price.js to display stock quote information

--- frontend/src/components/StockDetails/Price/Price.js
+++ frontend/src/components/StockDetails/Price/Price.js
@@ -1,18 +1,29 @@
 import React from 'react'
-
+import axios from 'axios'
 export default class Price extends React.Component{
     constructor(props){
         super(props)
-        this.state = { companyName:props.companyName}
+        this.state = { companyName:props.companyName, details : []}
     }
 
     componentDidMount(){
-        //here we call current stock price api
+        axios.get('/stockname/' + this.state.companyName)
+        .then((response)=> {
+            this.setState({details: response.data})
+        })
     }
 
     render(){
         return(
-            <h1>Price is: {this.state.companyName}</h1>
+            <div>
+                <h1>Company: {this.state.companyName}</h1>
+                <p>Current: ${this.state.details.c}</p>
+                <p>High: ${this.state.details.h}</p>
+                <p>Low: ${this.state.details.l}</p>
+                <p>Opening: ${this.state.details.o}</p>
+                <p> Previous Close: ${this.state.details.pc}</p>
+            </div>
+            
         )
     }
 }
