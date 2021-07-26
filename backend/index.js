f885a6bebb4ff9d93c2f1098f44bf878ddc2866f
Codubee <70549347+Codubee@users.noreply.github.com>
2021-07-26 10:44:01 -0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.get('/',(req,res)=>{
 app.get('/stockname/:NAME',(req, res)=>{
     const headers = {headers:{'Authorization':process.env.QUOTE_API_KEY}}
     let NAME = (req.params.NAME).toUpperCase();
-    axios.get(`https://finnhub.io/api/v1/quote?symbol=${NAME}&token=bv6jgev48v6s9eue2ndg`, headers)
+    axios.get(`https://finnhub.io/api/v1/quote?symbol=${NAME}&token=`, headers)
     .then(function(response){
         console.log(response);
         res.status(200).json(response.data);
